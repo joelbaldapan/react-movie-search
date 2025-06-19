fd2e2b871b28b34a3ace27519c126739bd5d3910
joelbaldapan <baldapan.joelangelo@gmail.com>
2025-06-20 01:28:17 +0800

    feat: add get movies and search functionality with api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,2 +1,17 @@
 const API_KEY: string = import.meta.env.VITE_TMDB_API_KEY;
-const BASE_URL: string = "https://api.themoviedb.org/3"
+const BASE_URL: string = "https://api.themoviedb.org/3";
+
+export async function getPopularMovies() {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  // console.log(data)
+  return data.results;
+}
+
+export async function searchMovies(query: string) {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${query}`
+  );
+  const data = await response.json();
+  return data.results;
+}
