58946d3ab2487ca7edb4f58c04349efa624ceb26
joelbaldapan <baldapan.joelangelo@gmail.com>
2025-06-20 02:14:49 +0800

    feat: add Context for favorites

---
+++ src/contexts/MovieContext.tsx
@@ -0,0 +1,52 @@
+import { createContext, useState, useEffect} from "react";
+import type { ReactNode } from "react";
+import type { Movie } from "../services/api";
+
+type MovieProviderProps = {
+  children: ReactNode;
+};
+
+type MovieContextProps = {
+  favorites: Movie[];
+  addToFavorites: (movie: Movie) => void;
+  removeFromFavorites: (movieId: number) => void;
+  isFavorite: (movieId: number) => boolean;
+};
+
+const MovieContext = createContext<MovieContextProps | undefined>(undefined);
+
+export const MovieProvider = ({ children }: MovieProviderProps) => {
+  const [favorites, setFavorites] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    const storedFavs = localStorage.getItem("favorites");
+
+    if (storedFavs) setFavorites(JSON.parse(storedFavs));
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+  }, [favorites]);
+
+
+  const addToFavorites = (movie: Movie) => {
+    setFavorites(prev => [...prev, movie])
+  }
+
+  const removeFromFavorites = (movieId: number) => {
+    setFavorites(prev => prev.filter(movie => movie.id !== movieId))
+  }
+
+  const isFavorite = (movieId: number) => {
+    return favorites.some(movie => movie.id === movieId)
+  }
+
+  const value = {
+    favorites,
+    addToFavorites,
+    removeFromFavorites,
+    isFavorite
+  }
+
+  return <MovieContext.Provider value={value}>{children}</MovieContext.Provider>;
+};
