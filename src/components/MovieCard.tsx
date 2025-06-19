17b8444a09533cd5e7f17a30eaa5190ec05b0206
joelbaldapan <baldapan.joelangelo@gmail.com>
2025-06-20 01:47:11 +0800

    fix: update MovieCard to render images and dates properly

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -1,10 +1,5 @@
 import "../css/MovieCard.css";
-
-type Movie = {
-  title: string;
-  release_date: string;
-  url?: string;
-};
+import type { Movie } from "../services/api";
 
 type MovieCardProps = {
   movie: Movie;
@@ -18,7 +13,10 @@ function MovieCard({ movie }: MovieCardProps) {
   return (
     <div className="movie-card">
       <div className="movie-poster">
-        <img src={movie.url} alt={movie.title} />
+        <img
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt={movie.title}
+        />
         <div className="movie-overlay">
           <button className="favorite-btn" onClick={onFavoriteClick}>
             ❤
@@ -27,7 +25,7 @@ function MovieCard({ movie }: MovieCardProps) {
       </div>
       <div className="movie-info">
         <h3>{movie.title}</h3>
-        <p>{movie.release_date}</p>
+        <p>{movie.release_date?.split("-")[0]}</p>
       </div>
     </div>
   );
