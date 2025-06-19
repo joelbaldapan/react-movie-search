3f3ae6a0821d8468fc7e5f863bd49ac95c39f88d
joelbaldapan <baldapan.joelangelo@gmail.com>
2025-06-20 00:08:46 +0800

    feat: add empty Favorites page text

---
+++ src/pages/Favorites.tsx
@@ -0,0 +1,10 @@
+function Favorite() {
+  return (
+    <div className="favorites-empty">
+      <h2>Favorite Movies List Empty!</h2>
+      <p>Start favoriting movies, and they will appear here.</p>
+    </div>
+  );
+}
+
+export default Favorite;
