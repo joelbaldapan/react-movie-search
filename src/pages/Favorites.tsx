a045e79fcdba196ead51581bea1f953603d90ce0
joelbaldapan <baldapan.joelangelo@gmail.com>
2025-06-20 00:33:54 +0800

    feat: add NavBar component

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -2,7 +2,7 @@ function Favorite() {
   return (
     <div className="favorites-empty">
       <h2>Favorite Movies List Empty!</h2>
-      <p>Start favoriting movies, and they will appear here.</p>
+      <p>Start favoriting movies, and they will appear here!</p>
     </div>
   );
 }
